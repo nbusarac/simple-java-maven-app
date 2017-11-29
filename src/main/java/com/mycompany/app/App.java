1fe2787515bfa531fa9f7856dbde5cc3a377f27b
Nenad Busarac <nbusarac@gmail.com>
2017-11-29 10:23:49 +0100

    source code change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        int n = 10;
+	while(n--){
+            
+        }
     }
 }
